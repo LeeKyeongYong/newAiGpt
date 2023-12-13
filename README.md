cc1f33ed9c03913d36f9e50e9ba3e7a6e9721ec8
이경용 <sleekydz86@icloud.com>
2023-12-14 00:34:12 +0900

    이미지를 이요한 Gpt생성
    이미지를 이요한 Gpt생성

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # newAiGpt
+
 Java를 기반으로 하며 Spring Boot 3와 JDK 17을 사용하여 구축되었습니다. 이 프로젝트는 ChatGPT, OpenAi Image, Midjourney, NewBing,다양한 기능을 지원합니다.
---
+++ src/main/java/com/chatgpt/newaigpt/NewAiGptApplication.java
@@ -0,0 +1,13 @@
+package com.chatgpt.newaigpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAiGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewAiGptApplication.class, args);
+    }
+
+}
